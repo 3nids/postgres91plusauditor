e376cca90a2fd1d65f24d6cf04cfe30c89c5da38
Rouzaud Denis <denis@Rouzauds-MacBook-Pro.local>
2013-04-11 15:05:49 +0200

    first

---
+++ __init__.py
@@ -0,0 +1,15 @@
+"""
+Qaudit 91 plus
+Denis Rouzaud
+denis.rouzaud@gmail.com
+April. 2013
+"""
+
+def classFactory(iface):
+    from qaudit91plus import Qaudit91plus
+    return Qaudit91plus(iface)
+    
+
+
+
+
