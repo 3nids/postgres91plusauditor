3bb4d8586360af5c96c5a10a8369250da7a436b1
Denis Rouzaud <denis.rouzaud@gmail.com>
2013-05-02 07:35:22 +0200

    emphasis changed lines

--- gui/differenceviewer.py
+++ gui/differenceviewer.py
@@ -1,5 +1,5 @@
 from PyQt4.QtCore import Qt
-from PyQt4.QtGui import QTableWidget, QTableWidgetItem, QAbstractItemView
+from PyQt4.QtGui import QTableWidget, QTableWidgetItem, QAbstractItemView, QBrush, QColor
 
 
 class DifferenceViewer(QTableWidget):
@@ -22,24 +22,32 @@ class DifferenceViewer(QTableWidget):
         self.setHorizontalHeaderItem(2, QTableWidgetItem(logRow.dateStr()))
         self.clearRows()
 
+        items = [0, 0, 0]
         for r, field in enumerate(layerFeature.fields()):
             self.insertRow(r)
 
-            item = QTableWidgetItem(field.name())
-            item.setFlags(Qt.ItemIsEnabled)
-            self.setItem(r, 0, item)
+            items[0] = QTableWidgetItem(field.name())
+            items[0].setFlags(Qt.ItemIsEnabled)
+            self.setItem(r, 0, items[0])
 
             currentValue = layerFeature.attribute(field.name()).toString()
-            item = QTableWidgetItem(currentValue)
-            item.setFlags(Qt.ItemIsEnabled)
-            self.setItem(r, 1, item)
+            items[1] = QTableWidgetItem(currentValue)
+            items[1].setFlags(Qt.ItemIsEnabled)
+            self.setItem(r, 1, items[1])
 
             logValue = logRow.getFieldValue(logRow.logData, field.name())
             if logValue is None:
                 logValue = ""
-            item = QTableWidgetItem(logValue)
-            item.setFlags(Qt.ItemIsEnabled)
-            self.setItem(r, 2, item)
+            items[2] = QTableWidgetItem(logValue)
+            items[2].setFlags(Qt.ItemIsEnabled)
+            self.setItem(r, 2, items[2])
+
+            if currentValue != logValue:
+                for item in items:
+                    item.setBackground(QBrush(QColor(240, 128, 128)))
+
+        self.adjustSize()
+        self.resizeColumnsToContents()
 
     def clearRows(self):
         while self.rowCount() > 0:
